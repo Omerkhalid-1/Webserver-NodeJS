8a122beb77ac5148b21c1d00b5c7c32d8b02e869
Omerkhalid-1 <omerkhalid0333@gmail.com>
2025-08-11 12:40:53 +0500

    Webserver Working

--- server.js
+++ server.js
@@ -9,11 +9,22 @@ const logEvents = require('./logEvents');
 class Emitter extends EventEmitter {}
 const myEmitter = new Emitter();
 
+
+const serveFile = async (filePath, contentType, res) => {
+    try {
+        const data = await fsPromises.readFile(filePath, 'utf8');
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+    } catch(err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Server Error');
+    }
+};
 // myEmitter.on('log', (msg) => logEvents(msg));
 
-const server = http.createServer(async (req, res) => {
-    console.log(req.url, req.method);
 
+/*
     let path;
 
     if (req.url === '/' || req.url === '/index.html') {
@@ -29,8 +40,74 @@ const server = http.createServer(async (req, res) => {
                 res.end(data);
             }
         });
-    
     }
+        */
+
+const server = http.createServer ((req, res) => {
+    console.log(req.url, req.method);
+    const extension = path.extname(req.url);
+    let contentType;
+    
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    // makes the .html not required in the URL
+    if (!extension && req.url.slice(-1) !== '/') {
+        filePath += '.html';
+    }
+
+    const fileExists = fs.existsSync(filePath);
+
+    if(fileExists){
+        serveFile(filePath, contentType, res);
+    } else{
+        // 404
+        // 301 redirect
+        switch(path.parse(filePath).base) {
+            case 'old-page.html':
+                res.writeHead(301, {Location: '/views/new-page.html'});
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, {Location: '/views/index.html'});
+                res.end();
+                break;
+            default: 
+                // serve a 404 response
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html' , res);
+
+    }
+}
 });
+
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
